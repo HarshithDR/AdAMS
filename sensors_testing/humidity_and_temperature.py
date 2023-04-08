4b04d33a230183fcbe172f2b901d6a4855fdef30
Harshith D R <harshithdr10@gmail.com>
2023-04-09 00:56:22 +0530

    added dht code

---
+++ sensors_testing/humidity_and_temperature.py
@@ -0,0 +1,9 @@
+import sys
+import Adafruit_DHT
+
+#gpio pin = 4
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
+
