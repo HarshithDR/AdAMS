2ec4315b46864f9302f0576b2fee4ca3a0dfd2de
Harshith D R <harshithdr10@gmail.com>
2023-04-23 12:49:50 +0530

    alcohol detection

--- sensors_testing/alcohol_detection.py
+++ sensors_testing/alcohol_detection.py
@@ -1,40 +1,35 @@
 import time
 import Adafruit_ADS1x15  # Import the ADS1x15 module
-import RPi.GPIO as GPIO
 
 # Define the ADS1115 ADC object
 adc = Adafruit_ADS1x15.ADS1115()
 
-# Define the GPIO pin connected to the MQ-3 sensor digital output
-MQ_PIN = 18
+# Define the voltage divider ratio for the MQ-3 sensor
+V_DIVIDER_RATIO = 4.2
 
-
-# Set up the GPIO pin for the MQ-3 sensor
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(MQ_PIN, GPIO.IN)
+# Define the load resistance for the MQ-3 sensor
+LOAD_RESISTANCE = 1.0  # kOhm
 
 # Define the calibration factor for the MQ-3 sensor
 CAL_FACTOR = 5.0
 
 # Main loop
 while True:
-    # Read the ADC values from channel 0 (A0) and channel 1 (A1)
+    # Read the ADC value from channel 0 (A0)
     adc_val_0 = adc.read_adc(0, gain=1)
-    adc_val_1 = adc.read_adc(1, gain=1)
 
-    # Convert the ADC values to voltage
+    # Convert the ADC value to voltage
     voltage_0 = adc_val_0 * 4.096 / 32767
-    voltage_1 = adc_val_1 * 4.096 / 32767
 
-    # Read the digital output from the MQ-3 sensor
-    mq_val = GPIO.input(MQ_PIN)
+    # Calculate the MQ-3 sensor resistance based on the voltage divider circuit
+    mq_resistance = (V_DIVIDER_RATIO * LOAD_RESISTANCE / voltage_0) - LOAD_RESISTANCE
 
-    # Calculate the alcohol concentration based on the MQ-3 sensor reading
-    alcohol_conc = mq_val * CAL_FACTOR
+    # Calculate the gas concentration based on the MQ-3 sensor resistance
+    gas_conc = CAL_FACTOR * mq_resistance / LOAD_RESISTANCE
 
     # Print the sensor readings to the console
-    print('ADC0: {0:.3f}V, ADC1: {1:.3f}V, MQ-3: {2}'.format(voltage_0, voltage_1, mq_val))
-    print('Alcohol concentration: {0:.3f} ppm'.format(alcohol_conc))
+    print('ADC0: {0:.3f}V, MQ-3 resistance: {1:.3f} kOhm'.format(voltage_0, mq_resistance))
+    print('Gas concentration: {0:.3f} ppm'.format(gas_conc))
 
     # Wait for 1 second before taking the next reading
     time.sleep(1)
