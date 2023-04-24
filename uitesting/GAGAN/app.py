8be047798980eafd325f3ae0e6001b1a42af03e3
gagan-ramesh <rockstar261001@gmail.com>
2023-04-24 23:58:19 +0530

    complete working map and graphs

--- uitesting/GAGAN/app.py
+++ uitesting/GAGAN/app.py
@@ -1,33 +1,45 @@
-from flask import Flask, render_template
-import time
-import random
-import matplotlib.pyplot as plt
-from io import BytesIO
 from flask import Flask, render_template, redirect, url_for
 import time
 import requests
 import base64
+from io import BytesIO
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
-data = [0] * 60
+data1 = [0] * 60
+data2 = [0] * 60
+data3 = [0] * 60
+count = 0
+
 @app.template_filter('b64encode')
 def b64encode_filter(s):
     return base64.b64encode(s).decode('utf-8')
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
 
 @app.route('/')
 def index():
-    global data
-    value= 10
-    data.pop(0)
-    data.append(value)
-    buffer = generate_graph()
-    buffer.seek(0)
-    # return render_template("index.html")
-    # return buffer.read()
+    global data1, data2, data3
+
+    value1 = 10  # example value for graph 1
+    value2 = 20  # example value for graph 2
+    value3 = 30  # example value for graph 3
+
+    data1.pop(0)
+    data1.append(value1)
+
+    data2.pop(0)
+    data2.append(value2)
+
+    data3.pop(0)
+    data3.append(value3)
+
+    buffer1 = generate_graph(data1)
+    buffer2 = generate_graph(data2)
+    buffer3 = generate_graph(data3)
+
+    buffer1.seek(0)
+    buffer2.seek(0)
+    buffer3.seek(0)
+
     # Set up the longitude and latitude coordinates for the location
     lat = 12.9724
     lon = 77.5806
@@ -39,27 +51,40 @@ def index():
     response = requests.get(url)
     image_data = response.content
 
-    # Render the template with the image data
-    return render_template('index.html', image_data=image_data)
-def generate_graph():
+    # Render the template with the image data and graph buffers
+    return render_template('index.html', image_data=image_data, buffer1=buffer1.read(), buffer2=buffer2.read(), buffer3=buffer3.read())
+
+@app.route("/update/<int:graph>/<int:value>")
+def update(graph, value):
+    global data1, data2, data3
+
+    if graph == 1:
+        data = data1
+    elif graph == 2:
+        data = data2
+    elif graph == 3:
+        data = data3
+
+    data.pop(0)
+    data.append(value)
+
+    buffer = generate_graph(data)
+    buffer.seek(0)
+
+    return redirect(url_for("index"))
+
+def generate_graph(data):
     plt.plot(range(len(data)), data)
     plt.xlabel('Time (s)')
     plt.ylabel('Value')
     plt.title('Data vs Time')
     buffer = BytesIO()
-    plt.savefig("static/images/image.png", format='png')
+    global count
+    count = count+1
+    plt.savefig("static/images/graph"+ str(count)+".png", format='png')
     buffer.seek(0)
     plt.close()
     return buffer
-@app.route("/update/<int:value>")
-def update(value):
-    global data
-    data.pop(0)
-    data.append(value)
-    buffer = generate_graph()
-    buffer.seek(0)
-    # return render_template("index.html")
-    # return buffer.read()
-    return redirect(url_for("index"))
+
 if __name__ == '__main__':
     app.run(debug=True)
