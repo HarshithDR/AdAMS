bf5a3f09edac44a474297cac5ffb3cf0ccb588ee
harshith <harshithdr10@gmail.com>
2023-05-03 00:24:24 +0530

    change alcohol testing code

--- hardware_integration/integrate.py
+++ hardware_integration/integrate.py
@@ -20,13 +20,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # mq3(alcohol sensor) config
-adc = Adafruit_ADS1x15.ADS1115()   # Define the ADS1115 ADC object
-# Define the voltage divider ratio for the MQ-3 sensor
-V_DIVIDER_RATIO = 4.2
-# Define the load resistance for the MQ-3 sensor
-LOAD_RESISTANCE = 1.0  # kOhm
-# Define the calibration factor for the MQ-3 sensor
-CAL_FACTOR = 5.0
+adc = Adafruit_ADS1x15.ADS1115()
+GAIN = 1
+MQ3_PIN = 0
 
 #temperature sensors config
 os.system('modprobe w1-gpio')
@@ -55,23 +51,11 @@ def impact():
     return True
 
 def alcohol():
-    # Read the ADC value from channel 0 (A0)
-    adc_val_0 = adc.read_adc(0, gain=1)
+    value = adc.read_adc(MQ3_PIN, gain=GAIN)
+    voltage = value / 32767.0 * 3.3  # Convert to voltage (assuming ADS1115 is set to +/- 4.096V range)
 
-    # Convert the ADC value to voltage
-    voltage_0 = adc_val_0 * 4.096 / 32767
-
-    # Calculate the MQ-3 sensor resistance based on the voltage divider circuit
-    mq_resistance = (V_DIVIDER_RATIO * LOAD_RESISTANCE / voltage_0) - LOAD_RESISTANCE
-
-    # Calculate the gas concentration based on the MQ-3 sensor resistance
-    gas_conc = CAL_FACTOR * mq_resistance / LOAD_RESISTANCE
-
-    # Print the sensor readings to the console
-    # print('ADC0: {0:.3f}V, MQ-3 resistance: {1:.3f} kOhm'.format(voltage_0, mq_resistance))
-    print('Gas concentration: {0:.3f} ppm'.format(gas_conc))
-
-    return gas_conc
+    print(f"Analog value: {value}, Voltage: {voltage:.2f}V")
+    return voltage
 
 def ambient_humidity_temperature():
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
--- sensors_testing/alcohol_detection.py
+++ sensors_testing/alcohol_detection.py
@@ -1,35 +1,14 @@
 import time
-import Adafruit_ADS1x15  # Import the ADS1x15 module
+import Adafruit_ADS1x15
 
-# Define the ADS1115 ADC object
 adc = Adafruit_ADS1x15.ADS1115()
+GAIN = 1
+MQ3_PIN = 0
 
-# Define the voltage divider ratio for the MQ-3 sensor
-V_DIVIDER_RATIO = 4.2
-
-# Define the load resistance for the MQ-3 sensor
-LOAD_RESISTANCE = 1.0  # kOhm
-
-# Define the calibration factor for the MQ-3 sensor
-CAL_FACTOR = 5.0
-
-# Main loop
 while True:
-    # Read the ADC value from channel 0 (A0)
-    adc_val_0 = adc.read_adc(0, gain=1)
+    value = adc.read_adc(MQ3_PIN, gain=GAIN)
+    voltage = value / 32767.0 * 3.3 # Convert to voltage (assuming ADS1115 is set to +/- 4.096V range)
 
-    # Convert the ADC value to voltage
-    voltage_0 = adc_val_0 * 4.096 / 32767
+    print(f"Analog value: {value}, Voltage: {voltage:.2f}V")
 
-    # Calculate the MQ-3 sensor resistance based on the voltage divider circuit
-    mq_resistance = (V_DIVIDER_RATIO * LOAD_RESISTANCE / voltage_0) - LOAD_RESISTANCE
-
-    # Calculate the gas concentration based on the MQ-3 sensor resistance
-    gas_conc = CAL_FACTOR * mq_resistance / LOAD_RESISTANCE
-
-    # Print the sensor readings to the console
-    print('ADC0: {0:.3f}V, MQ-3 resistance: {1:.3f} kOhm'.format(voltage_0, mq_resistance))
-    print('Gas concentration: {0:.3f} ppm'.format(gas_conc))
-
-    # Wait for 1 second before taking the next reading
-    time.sleep(1)
+    time.sleep(0.1) # Sleep for 100ms before taking the next measurement
